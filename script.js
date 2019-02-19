1cec0789c0100478bf4c0b86d1dbc416a90a273c
Diego Hernandez <diegohernandez@Diegos-MacBook-Pro.local>
2019-02-18 21:49:28 -0600

    test

--- script.js
+++ script.js
@@ -7,14 +7,38 @@ const layer = new L.StamenTileLayer('toner-background');
 map.addLayer(layer);
 
 // Request API data
-fetch(`${DATA_API_BASE}/alt-fuel-stations/v1.json?api_key=${DATA_API_KEY}&city=Chicago&state=IL`)
+// fetch(`${DATA_API_BASE}/alt-fuel-stations/v1.json?api_key=${DATA_API_KEY}&city=Chicago&state=IL`)
+//   .then(response => response.json())
+//   .then(data => {
+//     data.fuel_stations.forEach(station => {
+//       L.circleMarker([station.latitude, station.longitude], { stroke: false, fillColor: '#fff700', fillOpacity: 1 })
+//       .bindTooltip(`<h1>${station.station_name}</h1>`)
+//       .addTo(map);
+//     });
+//   })
+L.geoJSON(counties, {
+  onEachFeature: feat => {
+    console.log(feat);
+  }
+}).addTo(map)
+
+L.geoJSON()
+fetch(`./counties.geo.json`)
   .then(response => response.json())
   .then(data => {
-    data.fuel_stations.forEach(station => {
-      L.circleMarker([station.latitude, station.longitude], { stroke: false, fillColor: '#9f0', fillOpacity: 1 })
-      .bindTooltip(`<h1>${station.station_name}</h1>`)
-      .addTo(map);
-    });
+    console.log(data);
+    L.geoJSON(data, {
+      style: {
+        fillColor: 'red',
+        color: 'red',
+
+      }
+    }).addTo(map)
   })
+  .then(data => {
+});
+// fetch(`${DATA_API_BASE}/cleap/v1/gasoline_and_diesel_use?api_key=${DATA_API_KEY}&zip=60605`)
+//   .then(response => response.json())
+//   .then(console.log)
 
 // Create visualization
